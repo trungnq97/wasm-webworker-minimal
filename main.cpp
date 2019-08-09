14e163673d99b05c6e27bba7d56aa5776a4e2fd8
Trung Nguyen <trungnq97@gmail.com>
2019-08-09 21:17:53 +0100

    Send buf in WASM to ui thread

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <emscripten/bind.h>
+#include <emscripten/val.h>
+
+std::vector<unsigned char> myBuf {'H', 'e', 'l', 'l', 'o'};
 
 int myAdd(int a, int b) {
     for (auto i = 0; i < 100; i++) {
@@ -8,6 +13,12 @@ int myAdd(int a, int b) {
     return a + b;
 }
 
+emscripten::val getBytes() {
+    std::cout << myBuf.data() << std::endl;
+    return emscripten::val(emscripten::typed_memory_view(myBuf.size(), myBuf.data()));
+}
+
 EMSCRIPTEN_BINDINGS(MyModule) {
     emscripten::function("add", &myAdd);
+    emscripten::function("getBytes", &getBytes);
 }
--- my-worker.js
+++ my-worker.js
@@ -1,6 +1,9 @@
 self.Module = {
     onRuntimeInitialized: function() {
         console.log('SUM: ' + Module.add(1, 2));
+        var buf = Module.getBytes().slice().buffer;
+        self.postMessage(buf, [buf]);
+        console.log(buf);
     }
 };
 
